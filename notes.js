8a18910879dac8fa4fc1a0af81846ca5c1a64e63
f3mshep <superbiscuit@gmail.com>
2018-05-04 21:00:09 -0700

    poooooooooosh

--- notes.js
+++ notes.js
@@ -1,7 +1,26 @@
-console.log('starting notes.js')
+const fs = require("fs");
+
+console.log("starting notes.js");
 
 let addNote = (title, body) => {
   console.log('Adding note\n', title, body);
+  let notes;
+  let note = {
+    title,
+    body
+  }
+  try {
+    let notesString = fs.readFileSync("notes-data.json");
+    notes = JSON.parse(notesString);
+  } catch (error) {
+
+  }
+
+  let duplicateNotes = notes.filter((note) => note.title === title);
+  if (duplicateNotes.length === 0){
+    notes.push(note)
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes))
+  }
 };
 
 let getAll = () => {
